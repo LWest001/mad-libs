68e09f04780e7fb421baf84d5d5792da6f3d4741
Leo W <leo.westebbe@gmail.com>
2022-09-06 12:28:37 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Create a function that generates a random number to select from the word banks.
+
+//Create banks (arrays) of various parts of speech (words).
+
+const verbs = [""];
+const names = [""];
+const nouns = [""];
+const adjectives = [""];
+const body_parts = [""];
+const animals = [""];
+const numbers = {
+  //generate a random number
+};
+
+// Store fillable stories
+const story1 = `xVV
+vVD
+d`;
+const story2 = ``;
+const story3 = ``;
